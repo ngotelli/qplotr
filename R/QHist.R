c296488607eaa5adfe228f81c55c2a57dc497651
ngotelli <ngotelli@uvm.edu>
2024-10-06 21:41:58 -0400

    update qplot roxygen skeleton

--- R/QHist.R
+++ R/QHist.R
@@ -5,6 +5,25 @@
 # inputs:
 # outputs:
 ########################################
+#' Creates a Histogram of a Continuous Variable
+#'
+#' This function plots a histogram of a continuous variable,
+#' along with its mean, 95% confidence interval, and a smoothed
+#' density curve. A one-sample t-test compares the observed mean to
+#' an expectation from the null hypothesis (default h_0 = 0.0)
+#'
+#' @param x a vector of numeric values (integer or real)
+#' @param h_0 a constant numeric for the expectation of the
+#' null hypothesis. The default value is h_0 = 0.0.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' q_hist(x=runif(1000),h_0 = 0.50)
+#' }
+#'
 q_hist <- function (x = rnorm(100),  h_0 = 0) {
 
 # function body
@@ -14,27 +33,27 @@ q_hist <- function (x = rnorm(100),  h_0 = 0) {
     null <- paste("; H_0 =", round(h_0, digits = 3))
     z <- t.test(x, mu = h_0)$p.value
     p_val <- paste(": p =", signif(z, digits = 3))
-    
+
     z <- ggplot2::ggplot(df) +
-         aes(x = x) + 
-         geom_vline(xintercept = c(mean(df$x), 
-         quantile(df$x, prob = c(0.025, 0.975)), 
-         h_0), 
-         linetype = c("solid", "dashed", "dashed", "solid"), 
-         col = c("black", "black", "black", "red")) + 
+         aes(x = x) +
+         geom_vline(xintercept = c(mean(df$x),
+         quantile(df$x, prob = c(0.025, 0.975)),
+         h_0),
+         linetype = c("solid", "dashed", "dashed", "solid"),
+         col = c("black", "black", "black", "red")) +
          geom_density(col="grey80",fill="grey80") +
          geom_histogram(aes(y = after_stat(density)),
                         col = "black",
-                        fill = "wheat") + 
-         theme_classic(base_size = 20, 
-                       base_family="serif") + 
+                        fill = "wheat") +
+         theme_classic(base_size = 20,
+                       base_family="serif") +
          theme(plot.subtitle = element_text(size = 15)) +
          labs(subtitle = paste0(mean, var, null, p_val)) +
-         xlab("X Variable") + 
+         xlab("X Variable") +
          ylab("Density")
     plot(z)
-  
-  
+
+
 
 } # end of function q_hist
 # --------------------------------------
